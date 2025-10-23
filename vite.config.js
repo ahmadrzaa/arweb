dcaeeb9cc670d760cc07a544de47a80d6f8e10f9
ahmad raza <siddiquea725@gmail.com>
2025-10-23 17:44:48 +0300

    Fix: Vite base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // IMPORTANT for GitHub Pages project site:
-  base: '/arweb/',
+  base: '/arweb/',        // <— IMPORTANT for GitHub Pages project site
 })
