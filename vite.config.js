2f5a7f438dd83afb952456d84766e71f8801f4c7
ahmad raza <siddiquea725@gmail.com>
2025-10-23 17:32:58 +0300

    Set Vite base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import flowbiteReact from "flowbite-react/plugin/vite";
 
-// https://vite.dev/config/
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  title: 'MoizDev',
-  plugins: [react(), tailwindcss(), flowbiteReact()],
+  plugins: [react()],
+  // IMPORTANT for GitHub Pages project site:
+  base: '/arweb/',
 })
